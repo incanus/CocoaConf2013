79f73339f8c1958ddcbc434c1396641233b03cca
Justin R. Miller <incanus@codesorcery.net>
2013-08-14 16:24:12 -0700

    basic project cleanup

--- CocoaConf/MBXViewController.m
+++ CocoaConf/MBXViewController.m
@@ -8,22 +8,11 @@
 
 #import "MBXViewController.h"
 
-@interface MBXViewController ()
-
-@end
-
 @implementation MBXViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
